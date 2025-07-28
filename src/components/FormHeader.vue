5129b5de11e82444e93bc0649291cc5b470fa985
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-07-28 23:38:03 +0700

    chore : adjust the entry margin

--- src/components/FormHeader.vue
+++ src/components/FormHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center mt-12">
+  <div class="flex flex-col items-center mt-0">
     <h1 class="text-white text-4xl font-bold">Time to start your BVI company!</h1>
     <div class="text-gray-500 text-center mt-4">
       All questions below must be answered. Take your time, and if you need to
